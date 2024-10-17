a7b60a2a66cda6643ae3df12c0ece0dec154fc65
Jonathan Lezcano <jonathan.lezcano12@gmail.com>
2024-10-17 16:03:30 -0300

    Actualización de diseño y eliminación de JavaScript

--- cobertura_privada/core/urls.py
+++ cobertura_privada/core/urls.py
@@ -7,11 +7,12 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    path('', home, name='home'), 
+    path('', home, name='home'),  # Página de inicio
     path('bootstrap-home/', bootstrap_home, name='bootstrap_home'),  # Página con el template de Bootstrap
-    path('registrar_medico/', registrar_medico, name='registrar_medico'),
-    path('registrar_paciente/', views.registrar_paciente, name='registrar_paciente'),
-    path('buscar_medico/', buscar_medicos, name='buscar_medicos'),
-    path('medicos/', listar_medicos, name='listar_medicos'),
-    path('about/', views.about_me, name='about_me'),
+    path('registrar_medico/', registrar_medico, name='registrar_medico'),  # Página para registrar médico
+    path('registrar_paciente/', registrar_paciente, name='registrar_paciente'),  # Página para registrar paciente
+    path('registrar_cita/', registrar_cita, name='registrar_cita'),  # Página para registrar cita
+    path('buscar_medico/', buscar_medicos, name='buscar_medicos'),  # Página para buscar médicos
+    path('medicos/', listar_medicos, name='listar_medicos'),  # Página para listar médicos
+    path('about/', about_me, name='about_me'),  # Página de 'About Me
 ]
